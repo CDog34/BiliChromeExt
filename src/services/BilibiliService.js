01c6d97ea1c87e98268649ecabcebbe99129c690
Cao Junquan <i@cdog.me>
2017-02-14 01:03:08 +0800

    Fix https.

--- src/services/BilibiliService.js
+++ src/services/BilibiliService.js
@@ -1,6 +1,6 @@
 export class BilibiliService{
     static async loadBgm(bgmId){
-        const url=`http://bangumi.bilibili.com/jsonp/seasoninfo/${bgmId}.ver?_=${Date.now()}`;
+        const url=`//bangumi.bilibili.com/jsonp/seasoninfo/${bgmId}.ver?_=${Date.now()}`;
         const res= await fetch(url);
         const txt= await res.text();
         if (txt.search('seasonListCallback') !== -1) return await new Promise(resolve => (new Function('seasonListCallback',txt))(res => resolve(res)));
